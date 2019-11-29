3bfff16e7260f150ad748068f995e8258217bf42
Alejandro Velázquez <alum.avelazqueza@iesalixar.org>
2019-11-29 20:52:52 +0200

    primer paso u4 t1 ej1

---
+++ alixar/src/alixar/u4/t1/CuentaCorriente.java
@@ -0,0 +1,4 @@
+package alixar.u4.t1;
+
+public class CuentaCorriente {
+}
---
+++ alixar/src/alixar/u4/t1/Principal.java
@@ -0,0 +1,7 @@
+package alixar.u4.t1;
+
+public class Principal {
+    public static void main(String[] args) {
+       //  
+    }
+}
